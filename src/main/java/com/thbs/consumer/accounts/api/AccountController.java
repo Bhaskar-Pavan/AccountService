5ef844a83a3300a36316494c3b43dadb617db4a5
Bhaskar-Pavan <153126965+Bhaskar-Pavan@users.noreply.github.com>
2025-04-09 14:40:24 +0530

    Update AccountController.java

--- src/main/java/com/thbs/consumer/accounts/api/AccountController.java
+++ src/main/java/com/thbs/consumer/accounts/api/AccountController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/tca/v1/accounts")
+@RequestMapping
 public class AccountController implements AccountsApi {
 
     private final AccountService accountService;
